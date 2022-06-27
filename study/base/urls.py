76e9b9c921ce6ac4338163a18d1d77bdd2e6e9e7
neveszz <neveszalves@gmail.com>
2022-06-27 13:34:13 -0300

    crud

--- study/base/urls.py
+++ study/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home, room
+from .views import home, room, create_room, update_room, deleteRoom
 
 
 urlpatterns = [
     path('', home, name='home'),
-    path('room/<int:pk>', room, name='room')
+    path('room/<int:pk>', room, name='room'),
+    path('create_room/', create_room, name='create_room'),
+    path('update_room/<str:pk>/', update_room, name='update_room'),
+    path('delete_room/<str:pk>/', deleteRoom, name='delete_room')
 ]
